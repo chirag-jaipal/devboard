db702820810c41bec5ff37a793d7e143831d2fb0
Chirag Jaipal <chiragjaipal17@gmail.com>
2026-06-06 13:18:10 +0000

    Fix dashboard user typing

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -8,7 +8,7 @@ export default async function DashboardLayout({
   children: React.ReactNode;
 }) {
   const user = await getCurrentUser();
-  if (!user) {
+  if (!user?.id) {
     redirect("/signin");
   }
 
